45c2e6d415e1638768f02311dccd4f13d5a1e633
amansdali <77864882+amansdali@users.noreply.github.com>
2023-01-16 23:46:04 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -6,9 +6,13 @@
 import java.awt.Graphics;
 abstract class Card{
   private String name;
+  private int x;
+  private int y;
 //------------------------------------------------------------------------------
   Card(String name){
     this.name = name;
+    this.x = 300;
+    this.y = 400;
   }
 //------------------------------------------------------------------------------    
   public String getName(){
@@ -17,13 +21,25 @@ abstract class Card{
   public void setName(String name){
     this.name = name;
   }
+  public int getX(){
+    return this.x;
+  }
+  public void setX(int x){
+    this.x = x;
+  }
+  public int getY(){
+    return this.y;
+  }
+  public void setY(int y){
+    this.y = y;
+  }
 //------------------------------------------------------------------------------    
   abstract void play();
   abstract void draw(Graphics g);
   
   @Override
   public String toString(){
-    return ";"+this.name;
+    return this.name;
   }
 //------------------------------------------------------------------------------     
 }
