f080786d0e0ace5147db3d70d9a3d89495a79c51
amansdali <77864882+amansdali@users.noreply.github.com>
2023-01-17 23:15:53 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -4,11 +4,23 @@
  * @version 
  */
 import java.awt.Graphics;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 abstract class Card{
   private String name;
+  private String imageFile;
+  private BufferedImage picture;
 //------------------------------------------------------------------------------
-  Card(String name){
+  Card(String name, String imageFile){
     this.name = name;
+    this.imageFile = imageFile;
+    try {                
+      picture = ImageIO.read(new File(imageFile)); // load the picture from a file
+    } catch (IOException ex){
+      System.out.println("file not found");
+    } 
   }
 //------------------------------------------------------------------------------    
   public String getName(){
@@ -17,14 +29,20 @@ abstract class Card{
   public void setName(String name){
     this.name = name;
   }
-
+  public String getImageFile(){
+    return this.imageFile;
+  }
+  public void setImageFile(String imageFile){
+    this.imageFile=imageFile;
+  }
 //------------------------------------------------------------------------------    
   abstract void play();
-  abstract void draw(Graphics g, int x, int y);
-  
+//------------------------------------------------------------------------------  
   @Override
   public String toString(){
-    return this.name;
+    return this.name + ";" + this.imageFile;
+  }   
+  public void draw(Graphics g, int row, int col){
+    g.drawImage(picture,col*Const.GRID_SIZE,row*Const.GRID_SIZE,null);
   }
-//------------------------------------------------------------------------------     
 }
