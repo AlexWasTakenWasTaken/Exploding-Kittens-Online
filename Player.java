a03dbb221c5452c1194b0e5abcc0a26e3cd4c79d
Amanda <77864882+amansdali@users.noreply.github.com>
2023-01-18 13:52:19 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -8,16 +8,24 @@ import java.util.ArrayList;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Font;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 class Player {
-
   private ArrayList<Card> hand;
   private String name;
   private int playerNumber;
-  
-  Player(String name, String playerNumber){
+  private BufferedImage backOfCard;
+  Player(String name, int playerNumber){
     this.name = name;
-    this.playerNumber = Integer.parseInt(playerNumber);
+    this.playerNumber = playerNumber;
     this.hand = new ArrayList<Card>();
+    try {                
+      backOfCard = ImageIO.read(new File("Back"+playerNumber+".png")); // load the picture from a file
+    } catch (IOException ex){
+      System.out.println("file not found");
+    } 
   }
   public void addToHand(String card){
     String[] cardDetails = card.split(";");
@@ -58,9 +66,36 @@ class Player {
       hand.add(myCard);
     }
   }
-  public void draw(Graphics g){
-    for(int i=0; i<this.hand.size(); i++){
-      this.hand.get(i).draw(g, 10,i);
-    } 
+  public void draw(Graphics g, int mouseX, int mouseY){
+    int largeSize = 24;
+    Font largeFont = new Font("Arial", Font.PLAIN, largeSize);
+    g.setFont(largeFont); 
+    if(this.playerNumber == 1){
+      for(int i=0; i<this.hand.size(); i++){
+        if(this.hand.get(i).isUnderMouse(12,i,mouseX,mouseY)){
+          this.hand.get(i).draw(g, i,10);
+          this.hand.get(i).draw(g, 13,9);
+        }else{
+          this.hand.get(i).draw(g, i,11);
+        }
+        g.setColor(Color.black);
+        g.fillRect(0,14*Const.GRID_SIZE,10*Const.GRID_SIZE,2*Const.GRID_SIZE); 
+        g.setColor(Color.white);
+        g.drawString("Player 1: "+this.name, 0, 15*Const.GRID_SIZE);
+      }
+    }else if(this.playerNumber == 2){
+      for(int i=0; i<this.hand.size(); i++){
+        g.drawImage(this.backOfCard,18*Const.GRID_SIZE,(Const.ROWS-i)*Const.GRID_SIZE-Const.CARD_WIDTH,null);
+      }
+    }else if(this.playerNumber == 3){
+      for(int i=0; i<this.hand.size(); i++){
+        g.drawImage(this.backOfCard,(Const.COLUMNS-i)*Const.GRID_SIZE-Const.CARD_WIDTH,(-4)*Const.GRID_SIZE,null);
+      }
+    }else if(this.playerNumber == 4){
+      for(int i=0; i<this.hand.size(); i++){
+        g.drawImage(this.backOfCard,(-4)*Const.GRID_SIZE,i*Const.GRID_SIZE,null);
+      }
+    }
+    
   }  
 }
