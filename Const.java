825751625d84c092102c6f52009f0a71345b12a1
Amanda <77864882+amansdali@users.noreply.github.com>
2023-01-18 13:51:26 -0500

    Update Const.java

--- Const.java
+++ Const.java
@@ -6,7 +6,10 @@
 public class Const{
   final static int WIDTH = 1000;
   final static int HEIGHT = 800;
+  final static int COLUMNS = 20;
+  final static int ROWS = 16;
   final static int GRID_SIZE = 50;
   final static int CARD_WIDTH = 200;
   final static int CARD_HEIGHT = 300;
+  final static int FRAME_PERIOD = 20;
 }
