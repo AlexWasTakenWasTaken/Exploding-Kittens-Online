45df34c056aa0e7717d692eb6eb81c6163178955
amansdali <77864882+amansdali@users.noreply.github.com>
2023-01-17 23:16:17 -0500

    Update Const.java

--- Const.java
+++ Const.java
@@ -4,8 +4,9 @@
  * @version 
  */
 public class Const{
-  final static int WIDTH = 800;
-  final static int HEIGHT = 600;
-  final static int CARD_WIDTH = 30;
-  final static int CARD_HEIGHT = 60;
+  final static int WIDTH = 1000;
+  final static int HEIGHT = 800;
+  final static int GRID_SIZE = 50;
+  final static int CARD_WIDTH = 200;
+  final static int CARD_HEIGHT = 300;
 }
