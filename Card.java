d0c65712a8e6e9810643f87efb155800ef60249e
Amanda <77864882+amansdali@users.noreply.github.com>
2023-01-18 13:48:30 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -18,9 +18,7 @@ abstract class Card{
     this.imageFile = imageFile;
     try {                
       picture = ImageIO.read(new File(imageFile)); // load the picture from a file
-    } catch (IOException ex){
-      System.out.println("file not found");
-    } 
+    } catch (IOException ex){System.out.println("file not found");} 
   }
 //------------------------------------------------------------------------------    
   public String getName(){
@@ -38,11 +36,15 @@ abstract class Card{
 //------------------------------------------------------------------------------    
   abstract void play();
 //------------------------------------------------------------------------------  
+  public boolean isUnderMouse(int row, int col, int mouseX, int mouseY){
+    return mouseY>row*Const.GRID_SIZE && mouseY<row*Const.GRID_SIZE+Const.CARD_HEIGHT &&
+           mouseX>col*Const.GRID_SIZE && mouseX<(col+1)*Const.GRID_SIZE;
+  }
   @Override
   public String toString(){
     return this.name + ";" + this.imageFile;
   }   
-  public void draw(Graphics g, int row, int col){
+  public void draw(Graphics g, int col, int row){
     g.drawImage(picture,col*Const.GRID_SIZE,row*Const.GRID_SIZE,null);
   }
 }
